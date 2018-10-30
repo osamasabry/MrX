9c39ea927e13ab1b7b441d6c48f70fe1eb8a2958
osamasabry <osamasabry14@gmail.com>
2018-10-30 14:06:15 +0200

    add new Columns in Product and create all virtual with other models

--- Controller/productController.js
+++ Controller/productController.js
@@ -84,8 +84,17 @@ module.exports = {
 			newProduct.Product_Category_ID	 		= request.body.Product_Category_ID;
 			newProduct.Product_Estba7s	 			= request.body.Product_Estba7s;
 		 	newProduct.Product_Origin_Country_Code  = request.body.Product_Origin_Country_Code;
-        	newProduct.Product_Packing             	= request.body.Product_Packing;
-        	newProduct.Product_SupplierIds         	= request.body.Product_SupplierIds;
+        	newProduct.Product_Packing_Code         = request.body.Product_Packing_Code;
+        	newProduct.Product_Supplier_Codes       = request.body.Product_Supplier_Codes;
+        	newProduct.Product_MSDS         		= request.body.Product_MSDS;
+        	newProduct.Product_Classes_Code         = request.body.Product_Classes_Code;
+        	newProduct.Product_Assay         		= request.body.Product_Assay;
+        	newProduct.Product_Form_Code         	= request.body.Product_Form_Code;
+        	newProduct.Product_Certification        = request.body.Product_Certification;
+        	newProduct.Product_Release_Code         = request.body.Product_Release_Code;
+        	newProduct.Product_StorageType_Code     = request.body.Product_StorageType_Code;
+        	newProduct.Product_ProductCategory_Code = request.body.Product_ProductCategory_Code;
+			
 			newProduct.save(function(error, doneadd){
 				if(error){
 					return res.send({
@@ -102,28 +111,37 @@ module.exports = {
 
 		editProduct:function(req,res){
 			var newvalues = { $set: {
-				Product_Name 	     		: request.body.Product_Name,
-				Product_Chemical_Name   	: request.body.Product_Chemical_Name,
-				Product_Manufacturer 	    : request.body.Product_Manufacturer,
-				Product_Exporter 	     	: request.body.Product_Exporter,
-				Product_Molecular_Formula	: request.body.Product_Molecular_Formula,
-				Product_Molecular_Weight	: request.body.Product_Molecular_Weight,
-				Product_CAS_Number	 		: request.body.Product_CAS_Number,
-				Product_EC_Number	 		: request.body.Product_EC_Number,
-				Product_Appearance	 		: request.body.Product_Appearance,
-				Product_Active_Content	 	: request.body.Product_Active_Content,
-				Product_pH	 				: request.body.Product_pH,
-				Product_Sp_gravity	 		: request.body.Product_Sp_gravity,
-				Product_Chloride	 		: request.body.Product_Chloride,
-				Product_Iron	 			: request.body.Product_Iron,
-				Product_Phosphorous_Acid	: request.body.Product_Phosphorous_Acid,
-				Product_O_phosphate	 		: request.body.Product_O_phosphate,
-				Product_Hazen_color	 		: request.body.Product_Hazen_color,
-				Product_Category_ID	 		: request.body.Product_Category_ID,
-				Product_Estba7s	 			: request.body.Product_Estba7s,
-				Product_Origin_Country_Code : request.body.Product_Origin_Country_Code,
-        		Product_Packing             : request.body.Product_Packing,
-        		Product_SupplierIds         : request.body.Product_SupplierIds,
+				Product_Name 	     			  : request.body.Product_Name,
+				Product_Chemical_Name   		  : request.body.Product_Chemical_Name,
+				Product_Manufacturer 	    	  : request.body.Product_Manufacturer,
+				Product_Exporter 	     		  : request.body.Product_Exporter,
+				Product_Molecular_Formula		  : request.body.Product_Molecular_Formula,
+				Product_Molecular_Weight		  : request.body.Product_Molecular_Weight,
+				Product_CAS_Number	 			  : request.body.Product_CAS_Number,
+				Product_EC_Number	 			  : request.body.Product_EC_Number,
+				Product_Appearance	 			  : request.body.Product_Appearance,
+				Product_Active_Content	 		  : request.body.Product_Active_Content,
+				Product_pH	 					  : request.body.Product_pH,
+				Product_Sp_gravity	 			  : request.body.Product_Sp_gravity,
+				Product_Chloride	 			  : request.body.Product_Chloride,
+				Product_Iron	 				  : request.body.Product_Iron,
+				Product_Phosphorous_Acid		  : request.body.Product_Phosphorous_Acid,
+				Product_O_phosphate	 			  : request.body.Product_O_phosphate,
+				Product_Hazen_color	 			  : request.body.Product_Hazen_color,
+				Product_Category_ID	 			  : request.body.Product_Category_ID,
+				Product_Estba7s	 				  : request.body.Product_Estba7s,
+				Product_Origin_Country_Code 	  : request.body.Product_Origin_Country_Code,
+        		Product_Packing_Code              : request.body.Product_Packing_Code,
+        		Product_Supplier_Codes       	  : request.body.Product_Supplier_Codes,
+	        	Product_MSDS         			  : request.body.Product_MSDS,
+	        	Product_Classes_Code         	  : request.body.Product_Classes_Code,
+	        	Product_Assay         			  : request.body.Product_Assay,
+	        	Product_Form_Code         		  : request.body.Product_Form_Code,
+	        	Product_Certification        	  : request.body.Product_Certification,
+	        	Product_Release_Code         	  : request.body.Product_Release_Code,
+	        	Product_StorageType_Code     	  : request.body.Product_StorageType_Code,
+	        	Product_ProductCategory_Code 	  : request.body.Product_ProductCategory_Code,
+			
 			} };
 			var myquery = { Product_Code: request.body.Product_Code }; 
 			Product.findOneAndUpdate( myquery,newvalues, function(err, field) {
--- Model/product.js
+++ Model/product.js
@@ -2,36 +2,109 @@ var mongoose = require('mongoose');
 
 var Hcm_ProductSchema = mongoose.Schema({
    
-		Product_Code        		:Number,
-        Product_Name        		:String,
-        Product_Chemical_Name 		:String,
-        Product_Manufacturer        :String,
-        Product_Exporter            :String,
-        Product_Abbreviation   		:String,
-        Product_IsActive            :Number,
-        Product_Molecular_Formula 	:String,
-		Product_Molecular_Weight    :Number,
-        Product_CAS_Number 			:String,
-        Product_EC_Number 			:String,
-        Product_Appearance 			:String,
-        Product_Active_Content 		:String,
-        Product_pH 					:String,
-        Product_Sp_gravity 			:String,
-        Product_Chloride 			:String,
-        Product_Iron 				:String,
-        Product_Phosphorous_Acid 	:String,
-        Product_O_phosphate			:String,
-        Product_Hazen_color			:String,
-        Product_Category_ID			:[Number],
-        Product_Estba7s				:Number,
-        Product_Origin_Country_Code :Number,
-        Product_Packing             :[Number],
-        Product_Supplier_Codes      :[Number],
+		Product_Code        		    :Number,
+        Product_Name        	       	:String,
+        Product_Chemical_Name 		    :String,
+        Product_Manufacturer            :String,
+        Product_Exporter                :String,
+        Product_Abbreviation    		:String,
+        Product_IsActive                :Number,
+        Product_Molecular_Formula 	    :String,
+		Product_Molecular_Weight        :Number,
+        Product_CAS_Number 		       	:String,
+        Product_EC_Number 		    	:String,
+        Product_Appearance 		       	:String,
+        Product_Active_Content 	    	:String,
+        Product_pH 				     	:String,
+        Product_Sp_gravity 		       	:String,
+        Product_Chloride 		     	:String,
+        Product_Iron 			    	:String,
+        Product_Phosphorous_Acid       	:String,
+        Product_O_phosphate		       	:String,
+        Product_Hazen_color		       	:String,
+        Product_Category_ID			    :[Number],
+        Product_Estba7s				    :Number,
+        Product_Origin_Country_Code     :Number,
+        Product_Packing_Code            :Number,
+        Product_Supplier_Codes          :[Number],
+        Product_MSDS                    :String,
+        Product_Classes_Code            :Number,
+        Product_Assay                   :[String],
+        Product_Form_Code               :Number,
+        Product_Certification           :String,
+        Product_Release_Code            :Number,
+        Product_StorageType_Code        :Number,
+        Product_ProductCategory_Code    :[Number],
+
+},{
+    toJSON: { virtuals: true }
+});
+
+Hcm_ProductSchema.virtual('Category',{
+    ref: 'hcm_categories',
+    localField: 'Product_Category_ID',
+    foreignField: 'Category_ID',
+    justOne: false // for many-to-1 relationships
+});
+
+Hcm_ProductSchema.virtual('country',{
+    ref: 'hcm_countries',
+    localField: 'Product_Origin_Country_Code',
+    foreignField: 'Country_Code',
+    justOne: true // for many-to-1 relationships
+});
+
+Hcm_ProductSchema.virtual('productclass',{
+    ref: 'hcm_lut_classes',
+    localField: 'Product_Classes_Code',
+    foreignField: 'Class_Code',
+    justOne: true // for many-to-1 relationships
 });
 
 
+Hcm_ProductSchema.virtual('Supplier',{
+    ref: 'hcm_supplier',
+    localField: 'Product_Supplier_Codes',
+    foreignField: 'Supplier_Code',
+    justOne: false // for many-to-1 relationships
+});
 
 
+Hcm_ProductSchema.virtual('productform',{
+    ref: 'hcm_lut_form',
+    localField: 'Product_Form_Code',
+    foreignField: 'Form_Code',
+    justOne: true // for many-to-1 relationships
+});
+
+Hcm_ProductSchema.virtual('productpacking',{
+    ref: 'hcm_lut_packing',
+    localField: 'Product_Packing_Code',
+    foreignField: 'Packing_Code',
+    justOne: true // for many-to-1 relationships
+});
+
+
+Hcm_ProductSchema.virtual('productrelease',{
+    ref: 'hcm_lut_release',
+    localField: 'Product_Release_Code',
+    foreignField: 'Release_Code',
+    justOne: true // for many-to-1 relationships
+});
+
+Hcm_ProductSchema.virtual('productstrage',{
+    ref: 'hcm_lut_storage_type',
+    localField: 'Product_StorageType_Code',
+    foreignField: 'StorageType_Code',
+    justOne: true // for many-to-1 relationships
+});
+
+Hcm_ProductSchema.virtual('productcategory',{
+    ref: 'hcm_lut_porduct_category',
+    localField: 'Product_ProductCategory_Code',
+    foreignField: 'ProductCategory_Code',
+    justOne: true // for many-to-1 relationships
+});
 
 var Hcm_Product = module.exports = mongoose.model('hcm_product', Hcm_ProductSchema);
 
