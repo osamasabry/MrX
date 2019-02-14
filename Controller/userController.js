a150d7caad9dc4c6886d3de8d3e94d4bdc9a3bf7
osamasabry <osamasabry14@gmail.com>
2019-02-14 17:32:00 +0200

    hash password

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,18 +1,19 @@
 var User = require('../Model/user');
 // var crypto = require('crypto-js');
+var passwordHash = require('password-hash');
 
 
 module.exports = {
 
 
-	GetNextCode:function(request,res){
-		User.getLastCode(function(err,user){
-			if (user) 
-				res.send( Number(user.User_Code)+1);
-			else
-				res.send(1);
-		})
-	},
+	// GetNextCode:function(request,res){
+	// 	User.getLastCode(function(err,user){
+	// 		if (user) 
+	// 			res.send( Number(user.User_Code)+1);
+	// 		else
+	// 			res.send(1);
+	// 	})
+	// },
 
 	addUser:function(request,res){
 		User.getLastCode(function(err,user){
@@ -26,7 +27,7 @@ module.exports = {
 			var newUser = new User();
 			newUser.User_Code     	 	 = NextCode;
 			newUser.User_Name 	     	 = request.body.User_Name;
-			newUser.User_Password   	 = request.body.User_Password;
+			newUser.User_Password   	 = passwordHash.generate(request.body.User_Password);
 			newUser.User_DisplayName	 = request.body.User_DisplayName;
 			newUser.User_Permissions	 = request.body.User_Permissions;
 			newUser.User_IsActive	 	 = request.body.User_IsActive;
--- Model/user.js
+++ Model/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 // var bcrypt   = require('bcrypt-nodejs');
 
+var passwordHash = require('password-hash');
+
 var Hcm_UserSchema = mongoose.Schema({
    
 		User_Code        			:Number,
@@ -12,11 +14,19 @@ var Hcm_UserSchema = mongoose.Schema({
 });
 
 Hcm_UserSchema.methods.verifyPassword = function(password) {
-    if(password.localeCompare(this.User_Password) == 0)
+    if(passwordHash.verify(password,this.User_Password) == 1)
         return 1;
     else
         return 0;
 };
+
+
+// Hcm_UserSchema.methods.verifyPassword = function(password) {
+//     if(password.localeCompare(this.User_Password) == 0)
+//         return 1;
+//     else
+//         return 0;
+// };
 
 
 module.exports = mongoose.model('hcm_user', Hcm_UserSchema);
